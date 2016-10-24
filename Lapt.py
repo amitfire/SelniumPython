6f1bf457c4e3149fe0bd16b03cbcfd624e5143da
amit <amit.kumar@gslab.com>
2016-10-24 23:01:46 +0530

    Generica Class for Browser in Lapt.py

--- Lapt.py
+++ Lapt.py
@@ -4,52 +4,95 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import WebDriverException
 from prettytable import PrettyTable
 
-driver = webdriver.Firefox()
+driver = ''
 
-driver.implicitly_wait(10)
-driver.maximize_window()
-driver.get("https://www.flipkart.com")
-try:
-    search_field = driver.find_element_by_class_name("LM6RPg")   #getting the search field element by class name
-except NoSuchElementException as e:
-    print e
+class Browser():
+    global driver
+    def __init__(self,browser,url):
+        self.browser = browser
+        self.uri = url
+        if self.browser == 'FireFox':
+            drive= self.initate_firefox()
+        self.get_url()
+        return drive
 
-search_field.clear()
-search_field.send_keys("Laptops")                               #sending the search text to search field
-search_field.submit()
-laptop_list = []
-try:
-    fr = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div/div[2]/div[2]/div/section/ul')
-    items = fr.find_elements_by_tag_name("li")
-    for item in range(len(items)) :
-        if items[item].text == 'Popularity':
-            xpath = '/html/body/div[1]/div/div[2]/div[2]/div/div[2]/div[2]/div/section/ul/li['+str(item+1)+']'
-except WebDriverException as e :
-    print e
-print laptop_list
-page_number = 1
-while page_number <= 10 :
-    try:
-        driver.find_element_by_xpath(xpath).click()
-        x = driver.find_elements_by_class_name("_3wU53n")
-        for i in range(len(x)):
+    def initate_firefox(self):
+        print "Opening FireFox Browser.."
+        driver = webdriver.Firefox()
+        driver.implicitly_wait(10)
+        driver.maximize_window()
+        return driver
+
+    def get_url(self):
+        print "Entering url in the browser.." + self.uri
+        driver.get(url=self.uri)
+
+
+
+
+class Lister(Browser):
+    def __init__(self,link,explorer):
+        self.link = link
+        self.explorer = explorer
+
+
+    def set_up(self):
+        print "Setting up...\n"
+        self.driver = Browser(self.explorer,self.link)
+        self.run()
+
+    def run(self):
+        self.laptop_list = []
+        try:
+            self.search_field = self.driver.find_element_by_class_name("LM6RPg")
+            print "Found search field ....\n"
+            # getting the search field element by class name
+        except NoSuchElementException as e:
+            print e
+        self.search_field.clear()
+        self.search_field.send_keys("Laptops")
+        print "Entering Laptops in search Field\n"# sending the search text to search field
+        self.search_field.submit()
+        try:
+            fr = self.driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div/div[2]/div[2]/div/section/ul')
+            items = fr.find_elements_by_tag_name("li")
+            for item in range(len(items)):
+                if items[item].text == 'Popularity':
+                    xpath = '/html/body/div[1]/div/div[2]/div[2]/div/div[2]/div[2]/div/section/ul/li[' + str(item + 1) + ']'
+        except WebDriverException as e:
+            print e
+        page_number = 1
+        while page_number <= 10:
             try:
-                A = x[i].text
-                print A
-            except UnicodeEncodeError as e:
+                self.driver.find_element_by_xpath(xpath).click()
+                x = self.driver.find_elements_by_class_name("_3wU53n")
+                print "Getting all the Laptops name from "+ "page" + str(page_number)+ "\n"
+                for i in range(len(x)):
+                    try:
+                        A = x[i].text
+                        self.laptop_list.append(A)
+                    except UnicodeEncodeError as e:
+                        print e
+            except NoSuchElementException as e:
                 print e
-    except NoSuchElementException as e:
-        print e
-page_number += 1
+            page_number += 1
+        self.teardown()
 
-table = PrettyTable(['sr.no','Name of the Laptop'])
-for t in range(len(laptop_list)):
-    table.add_row([t,laptop_list[t]])
-print table
+    def teardown(self):
+        name_list = []
+        details_list = []
+        table = PrettyTable(['sr.no', 'Name of the Laptop','Details'])
+        for ele in range(len(self.laptop_list)):
+            name_list.append(str(self.laptop_list[ele].split('-')[0]))
+        for ele in range(len(self.laptop_list)):
+            details_list.append(str(self.laptop_list[ele].split('-')[1:3]))
+
+        for t in range(len(self.laptop_list)):
+            table.add_row([t, name_list[t],str(details_list[t]).replace('u','')])
+        print table
 
 
 
-
-
-
+a= Lister("https://www.flipkart.com","FireFox")
+a.set_up()
 
